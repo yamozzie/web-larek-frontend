242766242058a9d3194dd9ca2f0ec62dec57f853
Said Alistanov <alistanov.said@yandex.ru>
2024-10-09 12:14:39 +0300

    catalog rendering

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { CatalogModel } from './components/model/CatalogModel';
 import { BasketItemView } from './components/view/BasketItem';
 import { BasketView } from './components/view/Basket';
 import { API_URL } from './utils/constants';
+import { catalogItemView } from './components/view/CatalogItem'
 
 const events = new EventEmitter();
 const basketView = new BasketView(document.querySelector('.basket'));
@@ -33,3 +34,25 @@ events.on('ui:basket-add', () => {
 events.on('ui:basket-remove', () => {
     basketModel.delete
 });
+
+/* function renderCatalog(ids: string[]) {
+    const items = ids.map(id => {
+        const itemView = new catalogItemView(document.getElementById('#card-catalog') as HTMLTemplateElement, events);
+        return itemView.render(catalogModel.getProduct(id))
+    })
+} */
+function renderCatalog(products: IProduct[]) {
+    const itemTemplate = document.querySelector('#catalog-item-template') as HTMLTemplateElement;
+    
+    const items = products.map(product => {
+        const itemView = new catalogItemView(itemTemplate, events); 
+        return itemView.render(product);
+    });
+
+    items.forEach(item => {
+        itemTemplate.appendChild(item);
+    });
+}
+
+const products = catalogModel.items;
+renderCatalog(products);
